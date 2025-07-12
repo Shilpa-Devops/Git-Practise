456e393e302cfb71c20d8256c69479be10c6f1d9
SOURAV ROY <shilpa.roy@kapilshrivastavaG20outlook.onmicrosoft.com>
2025-07-12 16:27:50 +0530

    adding Pari code

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "dev-test1-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg-Paritosh" {
+  name     = "dev-Paritosh-rg"
+  location = "West Europe"
+}
